4291ca2f0754d7aaed98bda7270499e03fbfe954
Yurii Novikov <urichcoolx@gmail.com>
2025-04-01 01:07:24 +0200

    created-footer

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,24 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { CommonModule, NgFor } from '@angular/common';
 import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
 import { RouterLink } from '@angular/router';
 import { ProfileService } from '../../data/services/profile.service';
+import { firstValueFrom } from 'rxjs';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIconComponent, NgFor, SubscriberCardComponent, RouterLink, CommonModule],
+  imports: [SvgIconComponent, NgFor, SubscriberCardComponent, RouterLink, CommonModule, ImgUrlPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   profileService = inject(ProfileService);
 
   subscribers$ = this.profileService.getSubscribersShortList();
+
+  me = this.profileService.me
 
   menuItems: { label: string; icon: string; link: string }[] = [
     { label: 'My page', icon: 'home', link: '' },
     { label: 'Chats', icon: 'chats', link: 'chats' },
     { label: 'Search', icon: 'search', link: 'search' },
   ];
+
+  ngOnInit(): void {
+    firstValueFrom(this.profileService.getMe()) 
+  }
 }
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,27 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Profile } from '../interfaces/profile.interface';
 import { Pagebale } from '../interfaces/pagebale.interface';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
   http = inject(HttpClient);
-
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
+  me = signal<Profile | null>(null);
 
-  constructor() {}
 
-  getSubscribersShortList(){
-   return this.http.get<Pagebale<Profile>>(`${this.baseApiUrl}account/subscribers/`);
+
+  getSubscribersShortList() {
+    return this.http
+      .get<Pagebale<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(map((res) => res.items.slice(0, 3)));
   }
 
   getTestAccounts() {
-   return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
   }
 
-  getMe(){
-    return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
+  getMe() {
+    return this.http
+      .get<Profile>(`${this.baseApiUrl}account/me`)
+      .pipe(tap((res) => (this.me.set(res))));
   }
 }
