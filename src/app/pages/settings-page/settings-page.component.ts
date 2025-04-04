e2dc15915d4ba0f8871f6476f1ed4f30156df70a
Yurii Novikov <urichcoolx@gmail.com>
2025-04-04 02:09:45 +0200

    created-settings-forms

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [ProfileHeaderComponent],
+  imports: [ProfileHeaderComponent, ReactiveFormsModule],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss'
 })
 export class SettingsPageComponent {
+fb = inject(FormBuilder);
 
+form = this.fb.group({
+firstName: ['', Validators.required],
+lastName: ['', Validators.required],
+userName: ['', Validators.required],
+description: [''],
+stack: ['']
+})
 }
